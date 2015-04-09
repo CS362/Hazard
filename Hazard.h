2e8f733dc44239545c7036cd024ebcc67f91d1d3
Sam Gutierrez <sam_gutz@yahoo.com>
2015-04-08 18:20:11 -0700

    updated Hazard header and cpp files

--- Hazard.h
+++ Hazard.h
@@ -1,17 +1,23 @@
 #ifndef HAZARD_CLASS
 #define HAZARD_CLASS
-
+#include<string>
+using namespace std;
 class Hazard
 {
 	private: 
-		int hazard_ID;
+		int ID;
 		double radius;
 		Position hazard_position;
+		string type;
+		
 	public: 
-		void set_hazard_id();
+		void set_hazard_id(int);
 		int get_hazard_id();
 		void set_radius(double);
 		double get_radius();
+		void set_type(string);
+		string get_type();
+		
 };
 
 #endif
