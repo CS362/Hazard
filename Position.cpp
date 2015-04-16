eecbe3f9cd8ca9c0a10cf44293c41d5bb4aeaafc
unknown <jnanoff@cs408-5a.acad.fullerton.edu>
2015-04-15 17:10:38 -0700

    position files

---
+++ Position.cpp
@@ -0,0 +1,36 @@
+#include "Position.h"
+
+//constructors
+Position::Position()
+{
+	Latitude = 0;
+	Longitude = 0;
+}
+
+Position::Position(double x_long, double y_lat)
+{
+	Longitude = x_long;
+	Latitude = y_lat;
+}
+
+//SET functions
+void Position::setLatitude(double y)
+{
+	Latitude = y;
+}
+
+void Position::setLongitude(double x)
+{
+	Longitude = x;
+}
+
+//GET functions
+double Position::getLatitude()
+{
+	return Latitude;
+}
+
+double Position::getLongitude()
+{
+	return Longitude;
+}
